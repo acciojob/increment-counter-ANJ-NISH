0a65b0ac506cc6c6e7480d648a88b286c9c568a9
ANJ-NISH <155297183+ANJ-NISH@users.noreply.github.com>
2024-11-26 17:37:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+let btn=document.getElementById("incrementBtn");
+
+btn.addEventListener("click",
+					 function()
+					 {
+						 let pele=document.getElementById("counter");
+						 let value=pele.textContent;
+						 let val=parseInt(value);
+						 alert(value);
+						 pele.textContent=toString(val+1);
+					 })
